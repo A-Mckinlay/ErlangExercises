e1082ece5e2f693dfa543fbba084ed3d0dc23711
A-Mckinlay <a.mckinlay@dundee.ac.uk>
2017-11-26 20:03:34 +0000

    Extracted out the example code, created cleaner data version for results comparsion, completed map reduce implementation.

---
+++ ccharcount_helpers.erl
@@ -0,0 +1,35 @@
+-module(ccharcount_helpers).
+-compile(export_all).
+
+read_data(File_Name) ->
+  {ok, BIN} = file:read_file(File_Name),
+  Raw_List = binary_to_list(BIN),
+  Clean_List = string:to_lower(re:replace(Raw_List, "[^a-zA-Z]", "", [global, {return, list}])), %if it's not an alpahbetical character get rid of it
+  Bucket_Size = round( length(Clean_List) / 20 ),
+  Buckets = split(Clean_List, Bucket_Size),
+  io:fwrite("Loaded, cleaned and split~n"),
+  Buckets.
+
+split([],_)->[];
+split(List,Length)->
+  S1=string:substr(List,1,Length),
+  case length(List) > Length of
+    true->S2=string:substr(List,Length+1,length(List));
+    false->S2=[]
+  end,
+  [S1]++split(S2,Length).
+
+count(Ch, [],N)->N;
+count(Ch, [H|T],N) ->
+  case Ch==H of
+    true-> count(Ch,T,N+1);
+    false -> count(Ch,T,N)
+  end.
+
+
+join([],[])->[];
+join([],R)->R;
+join([H1 |T1],[H2|T2])->
+  {C,N}=H1,
+  {C1,N1}=H2,
+  [{C1,N+N1}]++join(T1,T2).
--- ccmapred.erl
+++ ccmapred.erl
@@ -1,7 +1,31 @@
 -module(ccmapred).
--export([read_data/1]).
+-export([begin_map_reduce/1, map_bucket/4, reduce_results/4]).
+-import(ccharcount_helpers, [count/3, join/2, read_data/1]).
 
-read_data(File_Name) ->
-  {ok, BIN} = file:read_file(File_Name),
-  List = binary_to_list(BIN),
-  io:format("list: ~p~n", [?MODULE]).
+begin_map_reduce(File_Name) ->
+  Buckets = read_data(File_Name),
+  start(Buckets, map_bucket, reduce_results).
+
+start(Buckets, Map_Func, Reduce_Func) ->
+  Reduce_PID = spawn(?MODULE, Reduce_Func, [self(), length(Buckets), 0, []]),
+  lists:foreach(fun(Bucket) -> spawn(?MODULE, Map_Func, [Bucket, lists:seq($a,$z),Reduce_PID, []]) end, Buckets),
+
+  receive
+    Results ->
+      Results
+  end.
+
+map_bucket(Bucket, [], Reduce_PID, Results) -> Reduce_PID ! Results;
+map_bucket(Bucket, [Letter|Rest_Of_Alphabet], Reduce_PID, Results) ->
+  Value = count(Letter, Bucket, 0),
+  New_Results = Results ++ [{[Letter], Value}],
+  map_bucket(Bucket, Rest_Of_Alphabet, Reduce_PID, New_Results).
+
+reduce_results(Main_PID, Num_Of_Buckets, Accum, Results) when Accum == Num_Of_Buckets ->
+  Main_PID ! Results;
+reduce_results(Main_PID, Num_Of_Buckets, Accum, Results) when Accum < Num_Of_Buckets ->
+  receive
+    Result_Set ->
+      New_Results = join(Results, Result_Set),
+      reduce_results(Main_PID, Num_Of_Buckets, Accum+1, New_Results)
+  end.
